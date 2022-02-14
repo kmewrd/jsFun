3c777d18a69666696190ce64b1f4d1956eda0531
Kim Ward <kmewrd@outlook.com>
2022-02-14 08:59:02 -0600

    Write function that makes a grocery list of toppings and quantities

--- prototypes/index.js
+++ prototypes/index.js
@@ -255,7 +255,16 @@ const cakePrompts = {
     //    ...etc
     // }
 
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = cakes.reduce((acc, cake) => {
+      cake.toppings.forEach(topping => {
+        if (!acc[topping]) {
+          acc[topping] = 1;
+        } else {
+          acc[topping] += 1;
+        }
+      });
+      return acc;
+    }, {});
     return result;
 
     // Annotation:
